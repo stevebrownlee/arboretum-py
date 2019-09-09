c77f841d8aad6f026c881c8e6d31545d824e20a3
Steve Brownlee <chortlehoort@gmail.com>
2019-09-09 08:38:08 -0500

    Working code added back in

--- actions/report.py
+++ actions/report.py
@@ -2,4 +2,7 @@ def build_facility_report(arboretum):
     for river in arboretum.rivers:
         print(f'River [{river.id}]')
 
+        for animal in river.animals:
+            print(f"{animal.id} ")
+
     input("\n\nPress any key to continue...")
